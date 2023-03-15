eb027f91af21d0d74affcd582c600b4b0dd6328d
piccadillygreens <piccadillygreens@gmail.com>
2023-03-16 00:40:03 +0530

    loader js

--- assets/js/main.js
+++ assets/js/main.js
@@ -222,6 +222,20 @@ window.addEventListener("scroll", reveal);
     loop: true,
     items: 1
   });
+  
+  // Loader
+  document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+      },1000);
+  }
+}
 
   
 
